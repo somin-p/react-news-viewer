3acac2c85e5c19601d63ffbd2c9bd18bbba21ee1
somin-p <sominpark.dev@gmail.com>
2021-10-25 17:31:12 +0900

    Feat: Manage each category with useState()

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,14 @@
+import { useCallback, useState } from "react";
 import Categories from "./components/categories";
 import NewsList from "./components/news_list";
 
 const App = () => {
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
   return (
     <>
-      <Categories />
-      <NewsList />
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
   );
 };
--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const categoryItems = [
   {
@@ -55,15 +55,32 @@ const CategoryItem = styled.div`
     color: #495057;
   }
 
+  ${(props) =>
+    props.active &&
+    css`
+      font-weight: 600;
+      border-bottom: 2px solid #22b8cf;
+      color: #22b8cf;
+      &:hover {
+        color: #3bc9db;
+      }
+    `}
+
   & + & {
     margin-left: 1rem;
   }
 `;
-const Categories = () => {
+const Categories = ({ onSelect, category }) => {
   return (
     <CategoriesBlock>
       {categoryItems.map((c) => (
-        <CategoryItem key={c.name}>{c.text}</CategoryItem>
+        <CategoryItem
+          key={c.name}
+          active={category === c.name}
+          onClick={() => onSelect(c.name)}
+        >
+          {c.text}
+        </CategoryItem>
       ))}
     </CategoriesBlock>
   );
